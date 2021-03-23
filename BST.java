114cf7c5a5447658fbb98f0a7047708c627ddb6b
Gregory Averianov <60460879+greg-aver@users.noreply.github.com>
2021-03-23 14:37:40 +0300

    Update BST.java

--- BST.java
+++ BST.java
@@ -374,4 +374,65 @@ class BST<T> {
         return Root;
     }
 
+        public ArrayList<BSTNode<T>> DeepAllNodes(int type_order) {
+        ArrayList<BSTNode<T>> listAllNodes = new ArrayList<>();
+        return switch (type_order) {
+            case 0 -> inOrderRecursiveDeepAllNode(listAllNodes, getRoot());
+            case 1 -> postOrderRecursiveDeepAllNode(listAllNodes, getRoot());
+            case 2 -> preOrderRecursiveDeepAllNode(listAllNodes, getRoot());
+            default -> listAllNodes;
+        };
+    }
+
+    private ArrayList<BSTNode<T>> inOrderRecursiveDeepAllNode(ArrayList<BSTNode<T>> listAllNodes, BSTNode<T> rootNode) {
+        if (rootNode.getLeftChild() != null) {
+            inOrderRecursiveDeepAllNode(listAllNodes, rootNode.getLeftChild());
+        }
+        listAllNodes.add(rootNode);
+        if (rootNode.getRightChild() != null) {
+            inOrderRecursiveDeepAllNode(listAllNodes, rootNode.getRightChild());
+        }
+        return listAllNodes;
+    }
+
+    private ArrayList<BSTNode<T>> postOrderRecursiveDeepAllNode(ArrayList<BSTNode<T>> listAllNodes, BSTNode<T> rootNode) {
+        if (rootNode.getLeftChild() != null) {
+            inOrderRecursiveDeepAllNode(listAllNodes, rootNode.getLeftChild());
+        }
+        if (rootNode.getRightChild() != null) {
+            inOrderRecursiveDeepAllNode(listAllNodes, rootNode.getRightChild());
+        }
+        listAllNodes.add(rootNode);
+        return listAllNodes;
+    }
+
+    private ArrayList<BSTNode<T>> preOrderRecursiveDeepAllNode(ArrayList<BSTNode<T>> listAllNodes, BSTNode<T> rootNode) {
+        listAllNodes.add(rootNode);
+        if (rootNode.getLeftChild() != null) {
+            inOrderRecursiveDeepAllNode(listAllNodes, rootNode.getLeftChild());
+        }
+        if (rootNode.getRightChild() != null) {
+            inOrderRecursiveDeepAllNode(listAllNodes, rootNode.getRightChild());
+        }
+        return listAllNodes;
+    }
+
+    public ArrayList<BSTNode<T>> WileAllNodes() {
+        ArrayList<BSTNode<T>> listAllNodes = new ArrayList<>();
+        LinkedList<BSTNode<T>> queue = new LinkedList<>();
+        BSTNode<T> node = getRoot();
+        do {
+            listAllNodes.add(node);
+            if (node.getLeftChild() != null) {
+                queue.add(node.getLeftChild());
+            }
+            if (node.getRightChild() != null) {
+                queue.add(node.getRightChild());
+            }
+            if (!queue.isEmpty()) {
+                node = queue.poll();
+            }
+        } while (!queue.isEmpty() || !listAllNodes.contains(node));
+        return listAllNodes;
+    }
 }
